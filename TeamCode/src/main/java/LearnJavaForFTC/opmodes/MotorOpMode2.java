1797166863b5277aee2f9e9f143ccee1794d7efc
raypratt <ray.pratt@jacksonpec.org>
2024-07-23 12:12:58 -0400

    Added RP exercises from Learn Java for FTC

---
+++ TeamCode/src/main/java/LearnJavaForFTC/opmodes/MotorOpMode2.java
@@ -0,0 +1,24 @@
+package LearnJavaForFTC.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import LearnJavaForFTC.mechanisms.ProgrammingBoard4;
+
+@TeleOp
+public class MotorOpMode2 extends OpMode{
+    ProgrammingBoard4 board = new ProgrammingBoard4();
+    @Override
+    public void init(){
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop(){
+        board.setMotorSpeed(0.5);
+        telemetry.addData("Motor Rotations", board.getMotorRotations());
+        telemetry.addData("Encoder Position", board.getCurrentPosition());
+        telemetry.addData("Ticks Per Rotation", board.getTicksPerRotation());
+        telemetry.addData("Gearing", board.getGearing());
+    }
+}
