cd842a31096464d0cda554f9024879282d047bc2
raypratt <ray.pratt@jacksonpec.org>
2024-10-03 15:09:53 -0400

    Added Color differentiaton in Auton.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -8,13 +8,25 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class Auton extends OpMode{
 
     Mechanisms mechs = new Mechanisms();
+    String color;
 
     @Override
     public void init(){
         mechs.init(hardwareMap);
     }
 
-
+    @Override
+    public void init_loop(){
+        telemetry.addLine("Press X for Blue, 0 for Red");
+        if (gamepad2.cross){
+            telemetry.addLine("Blue Selected");
+            color = "blue";
+        }
+        if (gamepad2.circle){
+            telemetry.addLine("Red Selected");
+            color = "red";
+        }
+    }
     @Override
     public void loop(){
 
